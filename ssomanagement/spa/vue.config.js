80cc93a6843b86a894385ed09c69b2f6d52d509e
wingzero0 <wingzzz2003@hotmail.com>
2021-07-30 17:33:48 +0800

    remove ssomangement spa; remove unused field in ssoserver messages_zh.properties
    add vue project in ssoserver
    
    add admin lte template
    
    remove spa project in sso server, use a pure npm project to keep track frontend package. the sso server login page is generated by thymeleaf, and it is not an easy job to co-work with single page web app.
    
    remove unused admin lte page

--- ssomanagement/spa/vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    devServer: {
-        port: 3000,
-        proxy: 'http://compiler:8082/ssomanagement/',
-    },
-    outputDir: '../src/main/resources/static/',
-    publicPath: process.env.NODE_ENV === 'production' ? '/ssomanagement/' : '/'
-}
